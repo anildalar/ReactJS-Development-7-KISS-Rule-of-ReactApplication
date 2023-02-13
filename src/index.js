0413eeaa65dbfa94edb0fef09733c6a20305d5bd
unknown <dollorinfotech@gmail.com>
2023-02-13 09:36:56 +0530

    commit 2

--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
+//IMport Area
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+
+// You can define a react compoent using 2 technique
+//1. Functional Compoent
+function A(props){ // We will recive a formal argument
+  let name="Anil"; //Data is availbable withing compoent
+  let surname2 = 'Thorecha';
+   //every function return somehting
+   return <h1>A {name} {props.surname} - <B surname={surname2} /> </h1>; //JSX
+}
+
+//2.class compoent
+//class Child extends Parent{ }
+class B extends React.Component{ //OOPs
+    //1. Properties/Variable
+    name='Pushpendra';
+
+
+    //2. Constructor
+
+    //3. methods
+    render(){
+      //Every function/method should return something
+      // In order to access any member of a class you have to use this.member
+      //this.member
+        return <span>B - {this.name} {this.props.surname} <C>Sharma</C></span>
+    }
+}
+
+//ES6 2015  Fat Arrow function
+
+let C =(props)=>{
+  let name="Vishnu";
+  console.log('Hi',props);
+  return <span>C - {name} {props.children} </span>
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+let sn = 'DOLOLR';
+root.render( <A surname={sn} /> );
+
+
+//3. Export 
